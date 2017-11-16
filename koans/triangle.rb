39abd7a5ac5884054e04665ed8797c76ea5a73e2
1628574 <1628574@stud.hs-mannheim.de>
2017-11-16 12:45:03 +0100

    👣 Step 161 done

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a==b && b==c
+  if a<=0 || b<=0 || c<=0
+      raise TriangleError.new()
+  elsif a+b<=c || a+c<=b || b+c<=a
+      raise TriangleError.new()
+  elsif a==b && b==c
     :equilateral
   elsif a==b || b==c || a==c
     :isosceles
