a09c4d4b1acd2a35ea2f2f0cde3438c013ead128
1628574 <1628574@stud.hs-mannheim.de>
2018-01-09 18:59:19 +0100

    Klammerneuformatierung

--- crypter/crypto.c
+++ crypter/crypto.c
@@ -7,8 +7,7 @@
 #define ENCRYPT_MODUS 0
 #define DECRYPT_MODUS 1
 
-int crypt(KEY key, const char* in, char* out, int modus)
-{
+int crypt(KEY key, const char* in, char* out, int modus){
         const char* inPos;
         const char* keyPos = key.chars;
         char* outPos = out;
@@ -16,32 +15,26 @@ int crypt(KEY key, const char* in, char* out, int modus)
         int firstKeyRun = 1;
 
         /** Check if the key is to short **/
-        if(!strlen(key.chars))
-        {
+        if(!strlen(key.chars)) {
                 return E_KEY_TOO_SHORT;
         }
 
-        for(inPos = in; *inPos; ++inPos)
-        {
+        for(inPos = in; *inPos; ++inPos) {
                 /** Reuse the key, if the input is longer than the key **/
-                if(!*keyPos)
-                {
+                if(!*keyPos) {
                         keyPos = key.chars;
                         /** We do not have to check the key again **/
                         firstKeyRun = 0;
                 }
 
                 /** Check on the first run, if the key contain illegal characters **/
-                if(firstKeyRun && !strchr(KEY_CHARACTERS, *keyPos))
-                {
+                if(firstKeyRun && !strchr(KEY_CHARACTERS, *keyPos)) {
                         return E_KEY_ILLEGAL_CHAR;
                 }
 
-                if(modus == ENCRYPT_MODUS)
-                {
+                if(modus == ENCRYPT_MODUS) {
                         /** Check if the message contain illegal characters **/
-                        if(!strchr(MESSAGE_CHARACTERS, *inPos))
-                        {
+                        if(!strchr(MESSAGE_CHARACTERS, *inPos)) {
                                 return E_MESSAGE_ILLEGAL_CHAR;
                         }
 
@@ -50,11 +43,9 @@ int crypt(KEY key, const char* in, char* out, int modus)
                               ((strchr(KEY_CHARACTERS, *keyPos) - (KEY_CHARACTERS)) + 1);
                         *outPos = CYPHER_CHARACTERS[pos];
                 }
-                else
-                {
+                else{
                         /** Check if the cypher contain illegal characters **/
-                        if(!strchr(CYPHER_CHARACTERS, *inPos))
-                        {
+                        if(!strchr(CYPHER_CHARACTERS, *inPos)) {
                                 return E_CYPHER_ILLEGAL_CHAR;
                         }
 
@@ -71,12 +62,10 @@ int crypt(KEY key, const char* in, char* out, int modus)
         return 0;
 }
 
-int encrypt(KEY key, const char* input, char* output)
-{
+int encrypt(KEY key, const char* input, char* output){
         return crypt(key, input, output, ENCRYPT_MODUS);
 }
 
-int decrypt(KEY key, const char* cypherText, char* output)
-{
+int decrypt(KEY key, const char* cypherText, char* output){
         return crypt(key, cypherText, output, DECRYPT_MODUS);
 }
